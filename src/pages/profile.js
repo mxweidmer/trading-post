ac836e92e21e80e4588e79747657416f2a5e61cf
Susie <45341415+Haldevel@users.noreply.github.com>
2019-05-02 11:40:38 -0700

    pass user data for profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,54 +1,92 @@
-import React from 'react'
-import { Profile, List } from '../components/ProfileCard'
-import {Row} from '../components/Grid'
+//import React from 'react'
+import React, { Component } from "react";
+import { List } from '../components/ProfileCard';
+import { Row } from '../components/Grid';
+import API from "../utils/API";
 
-function Profilepage() {
-    return (
-        <div>
-            <br/>
-            <Profile
-                image="https://i.imgur.com/H37kxPH.jpg"
-                username="Doggo McPuppers"
-                description="Average doggo, normal doggo things in progress." />
+class Profile extends Component {
 
-            <Row>
-            <List
-                listTitle="Posts"
-                items={
-                    [{
-                        id: "1",
-                        name: "burrito",
-                        link: "#"
+    state = {
+        _id: "5cca717879f2d60017aed66e",
+        isLoaded: false,
+        error: null,
+        firstName: "",
+        lastName: "",
+        city: "",
+        state: "",
+        phone: "",
+        items: [],
+        wishlist: [],
+        searchTerm: "",
+        pg: "Profile",
+        categories: ['General', 'Books', 'Electronics', 'Jewerly', 'Tools', 'Clothing', 'Furniture', 'Games', 'Sports Equipment', 'Appliances']
+    };
 
-                    },
-                    {
-                        id: "2",
-                        name: "purrito",
-                        link: "#"
-                    }]
-                }
+    componentDidMount() {
+        this.loadUser(this.state._id);
+    }
 
-            />
-            <List 
-            listTitle="Wishlist"
-            items={
-                [{
-                    id: "3",
-                    name: "Bowl of Cereal",
-                    link: "#"
+    loadUser = (id) => {
+        API.getUserInfo(id)
+            .then(
+                res => {
+                    this.setState({ returnedItems: res.data, isLoaded: true })
+                    console.log(res.data)
                 },
-                {
-                    id: "4",
-                    name:"Overcooked, raw sushi",
-                    link: "#"
+                error => {
+                    this.setState({ isLoaded: true, error });
                 }
-            ]}
-            />
-            </Row>
+            )
+        //.catch(err => console.log(err));
+    };
 
+    render() {
+        return (
+            <div>
+                <br />
+               {/*  <Profile
+                    image="https://i.imgur.com/H37kxPH.jpg"
+                    username="Doggo McPuppers"
+                    description="Average doggo, normal doggo things in progress." /> */}
 
-        </div>
-    )
+                <Row>
+                    <List
+                        listTitle="Posts"
+                        items={
+                            [{
+                                id: "1",
+                                name: "burrito",
+                                link: "#"
+
+                            },
+                            {
+                                id: "2",
+                                name: "purrito",
+                                link: "#"
+                            }]
+                        }
+
+                    />
+                    <List
+                        listTitle="Wishlist"
+                        items={
+                            [{
+                                id: "3",
+                                name: "Bowl of Cereal",
+                                link: "#"
+                            },
+                            {
+                                id: "4",
+                                name: "Overcooked, raw sushi",
+                                link: "#"
+                            }
+                            ]}
+                    />
+                </Row>
+            </div>
+        )
+    }
+
 }
 
-export default Profilepage;
+export default Profile;

f1ba217508437092d39939e2e136dc7393783f63
atiftariq786 <atiftariq786@hotmail.com>
2019-05-02 14:32:31 -0700

    code added  auth sessions and user info

--- src/App.js
+++ src/App.js
@@ -4,27 +4,46 @@ import React from 'react';
 //import {SearchBar, SeachBtn, SearchCategory} from './components/Search'
 //import { Row } from './components/Grid';
 //import Postform from './components/Postform';
-//=================AWS======================
+//===================== AWS Start ====================================
 import Amplify from '@aws-amplify/core';
 import aws_exports from "./aws-exports";
 import { withAuthenticator } from 'aws-amplify-react';
 import { Auth } from 'aws-amplify';
 
 Amplify.configure(aws_exports);
-//=======================================
+//======================= AWS End ==============================
 
 
 function App() {
- //======== Grabing User Information from AWS ===========
- 
+ //======== Grabing User Information from AWS ===================
+  let username = null
   Auth.currentAuthenticatedUser({
-    bypassCache: true  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-  }).then(user =>console.log(user)
-   
+    bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+  }).then(user => {
+  console.log(user)
+  
+  username = user.username
+  
+  console.log(username)
+
+})
+.catch(err => console.log(err));
+//============ Grabbing session information from AWS ==============
+
+
+Auth.currentSession()
+.then(data => 
+  
+  console.log(data)
 
 )
 .catch(err => console.log(err));
-//============AWS END =======================  
+
+
+
+
+
+//========================= AWS END ===============================  
 return (
     
     <h1> Welcome to Trading Dashboard</h1>

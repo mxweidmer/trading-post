99d1dce430a582d9c800b10c8d6d052fb2d048b8
Sam Thompson <electronic62794@yahoo.com>
2019-05-01 20:19:23 -0700

    profile page finished, need to style a little, and adjust props to match API results when we get that set up

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,19 +1,54 @@
-import  React from 'react'
-import {Profile, List} from '../components/ProfileCard'
+import React from 'react'
+import { Profile, List } from '../components/ProfileCard'
+import {Row} from '../components/Grid'
 
-function Profile (){
-    return(
+function Profilepage() {
+    return (
         <div>
-        <Profile 
-        image="https://i.imgur.com/H37kxPH.jpg"
-        username="Doggo McPuppers"
-        description="Average doggo, normal doggo things in progress."/>
+            <br/>
+            <Profile
+                image="https://i.imgur.com/H37kxPH.jpg"
+                username="Doggo McPuppers"
+                description="Average doggo, normal doggo things in progress." />
 
+            <Row>
+            <List
+                listTitle="Posts"
+                items={
+                    [{
+                        id: "1",
+                        name: "burrito",
+                        link: "#"
 
-        <List 
-        ListTitle="Posts"
-        />
+                    },
+                    {
+                        id: "2",
+                        name: "purrito",
+                        link: "#"
+                    }]
+                }
+
+            />
+            <List 
+            listTitle="Wishlist"
+            items={
+                [{
+                    id: "3",
+                    name: "Bowl of Cereal",
+                    link: "#"
+                },
+                {
+                    id: "4",
+                    name:"Overcooked, raw sushi",
+                    link: "#"
+                }
+            ]}
+            />
+            </Row>
+
 
         </div>
     )
 }
+
+export default Profilepage;

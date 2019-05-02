9d8d806ec4821d490d6e2bf3fbdccc9f5fa297c2
Susie <45341415+Haldevel@users.noreply.github.com>
2019-05-02 09:55:10 -0700

    pass the data to the main page and add nomatch page

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,10 +1,23 @@
 import axios from "axios";
 
 export default {
-    getUserItem: function() {
-        return axios.get("Localhost:3000/api/users/:id")
+    //get most recently added items on the landing page
+    getRecentItems: function () {
+        return axios.get("https://trading-post-server.herokuapp.com/api/items/dateDown")
     },
-    createNewItem: function() {
+    // Gets items based on a search term/category - neds more work
+    searchItem: function (search) {
+        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + search); //change the url based on our api
+    },
+    //get request for the create a new item form
+    createNewItem: function () {
+        return axios.get("https://trading-post-server.herokuapp.com/api/items/:userid")
+    },
+    //get request to receive a user's ifo with his/her items and the wishlist
+    createNewItem: function () {
+        return axios.get("https://trading-post-server.herokuapp.com/api/users/:id")
+    },
+    /* createNewItem: function() {
         return axios.post("Localhost:3000/api/items/:userid")
     },
     updateItem: function() {
@@ -12,6 +25,6 @@ export default {
     },
     deleteItem: function() {
         return axios.delete("Localhost:3000/api/items/:itemId")
-    }
+    } */
 
 }

678b62ded850d0069787237d49324388ecd0fb45
Susie <45341415+Haldevel@users.noreply.github.com>
2019-05-03 11:24:55 -0700

    add signin and signup skeletons

---
+++ src/pages/signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+
+function signin() {
+    return (
+      <div >
+              <h1>Sign in page</h1>                         
+             
+      </div> 
+    ); 
+  }
+  
+  export default signin;

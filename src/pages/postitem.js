4b8fa63aae5523a43d4452ab41056cc8191a52e6
Susie <45341415+Haldevel@users.noreply.github.com>
2019-05-03 12:55:53 -0700

    made postitem main class to add item

--- src/pages/postitem.js
+++ src/pages/postitem.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import Postform from '../components/Postform'
-import PostFormBtn from '../components/PostFormBtn'
+import PostFormBtn from '../components/PostFormBtn';
+//import { Container} from "../Grid";
 
-import { Row } from '../components/Grid';
+import { Container, Row } from '../components/Grid';
 import API from "../utils/API";
 
 class PostItem extends Component {
@@ -16,19 +17,67 @@ class PostItem extends Component {
     categories: ['General', 'Books', 'Electronics', 'Jewerly', 'Tools', 'Clothing', 'Furniture', 'Games', 'Sports Equipment', 'Appliances']
   };
 
-  
+
 
   render() {
     return (
-      <div>
 
+      <Container>
+        <div className="formCont">
+          <h2> Post your Item </h2>
+          <div className="row">
+            <div className="input-field col s6">
 
-        <div className="container">
+              <input id="title" type="text" value={this.state.title} required />
+              <label for="title">Add a Title</label>
+            </div>
+            <div className="input-field col s6">
 
+              <input id="picture" type="text" required />
+              <label for="picture">Add a Picture</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
 
-          <Postform> </Postform>  <PostFormBtn></PostFormBtn>
+              <input id="Discription" type="text" required />
+              <label for="Discription">Description</label>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+
+                <input id="Condition" type="text" required />
+                <label for="Condition">Condition</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="categorydrop">
+
+                <select className="browser-default">
+
+                  <option value="General">General</option>
+                  <option value="Books">Books</option>
+                  <option value="Electronics">Electronics</option>
+                  <option value="Jewerly">Jewerly</option>
+                  <option value="Tools">Tools</option>
+                  <option value="Clothing">Clothing</option>
+                  <option value="Furniture">Furniture</option>
+                  <option value="Games">Games</option>
+                  <option value="Sports Equipment">Sports Equipment</option>
+                  <option value="Appliances">Appliances</option>
+                </select>
+                <label for="categorydrop">Choose a category</label>
+              </div>
+            </div>
+            <button> </button>
+          </div>
+
         </div>
-      </div>
+      </Container>
+
+
+      /*  <Postform> </Postform>  <PostFormBtn></PostFormBtn> */
+
     )
   }
 

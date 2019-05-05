20697d1329446648b6599f4fe97573ef26f8bec2
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-05-05 15:54:15 -0700

    fixing api links (switch from localhost to heroku link)

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,28 +2,28 @@ import axios from "axios";
 
 export default {
     user: function () {
-        return axios.get("http://localhost:8080/api/auth/user", { crossdomain: true, withCredentials: true })
+        return axios.get("https://trading-post-server.herokuapp.com/api/auth/user", { crossdomain: true, withCredentials: true })
     },
 
     signup: function (signupData) {
-        return axios.post("http://localhost:8080/api/auth/signup", signupData)
+        return axios.post("https://trading-post-server.herokuapp.com/api/auth/signup", signupData)
     },
 
     signin: function (signinData) {
-        return axios.post("http://localhost:8080/api/auth/login", signinData)
+        return axios.post("https://trading-post-server.herokuapp.com/api/auth/login", signinData)
     },
 
     //get most recently added items on the landing page
 
     // http://fathomless-sands-76947.herokuapp.com/
     getRecentItems: function () {
-        return axios.get("http://localhost:8080/api/items/dateDown")
+        return axios.get("https://trading-post-server.herokuapp.com/api/items/dateDown")
     },
     //post request for the create a new item
     createNewItem: function (userId, postData) {
-        //return axios.post("http://localhost:8080/api/items/:userid", postData)
+        //return axios.post("https://trading-post-server.herokuapp.com/api/items/:userid", postData)
         console.log(postData);
-        return axios.post(`http://localhost:8080/api/items/${userId}`, postData)
+        return axios.post(`https://trading-post-server.herokuapp.com/api/items/${userId}`, postData)
         /* example
         {	
         "_owner": "5cc8da15ce98f8f39fccd613",
@@ -38,11 +38,11 @@ export default {
     //getUserInfo: function (userid) {
     //get request to receive a user's ifo with his/her items and the wishlist
     users: function () {
-        return axios.get("http://localhost:8080/api/auth/users")
+        return axios.get("https://trading-post-server.herokuapp.com/api/auth/users")
     },
 
     getUserInfo: function (userid) {
-        return axios.get("http://localhost:8080/api/users/" + userid)
+        return axios.get("https://trading-post-server.herokuapp.com/api/users/" + userid)
         //return axios.get("https://trading-post-server.herokuapp.com/api/users/cca717879f2d60017aed66e");
     },
     //get request to receive items based on the category and the search term
@@ -51,18 +51,18 @@ export default {
          searchTerm = "Apple"; */
         console.log("category: " + category);
         console.log("searchTerm: " + searchTerm);
-        return axios.get("http://localhost:8080/api/categories/" + category + "/" + searchTerm)
+        return axios.get("https://trading-post-server.herokuapp.com/api/categories/" + category + "/" + searchTerm)
     },
     //create a new user (post method for signing up)
     /*
     signup: function(signupData) {
-        return axios.post("http://localhost:8080/api/auth/users",signupData)
+        return axios.post("https://trading-post-server.herokuapp.com/api/auth/users",signupData)
     },
     */
 
     //get request for update an item form
     updateItem: function (itemid) {
-        return axios.get("http://localhost:8080/api/items/single/" + itemid);
+        return axios.get("https://trading-post-server.herokuapp.com/api/items/single/" + itemid);
         /* example
         {
         title: "A ring",
@@ -80,7 +80,7 @@ export default {
     },
     //put request for updating an existing item
     updateExistingItem: function (itemid, postData) {
-        return axios.put("http://localhost:8080/api/items/single/" + itemid, postData)
+        return axios.put("https://trading-post-server.herokuapp.com/api/items/single/" + itemid, postData)
 
         //on the backend we have to make sure we update only four fields for a given itemid!
         /* example
@@ -96,7 +96,7 @@ export default {
     },
 
     getItemData: function (itemId) {
-        return axios.get("http://localhost:8080/api/items/single/" + itemId)
+        return axios.get("https://trading-post-server.herokuapp.com/api/items/single/" + itemId)
     },
 
 
@@ -107,7 +107,7 @@ export default {
     deleteItem: function(userId, itemId) {
         console.log(userId)
         console.log(itemId)
-        return axios.delete(`http://localhost:8080/api/items/${userId}/${itemId}`)
+        return axios.delete(`https://trading-post-server.herokuapp.com/api/items/${userId}/${itemId}`)
     } 
 
 }
